ab909396e0724dd67b9ca8ea8c7afebdb591f64b
Fred Porter <me@fredporter.com>
2026-01-29 16:18:27 +1000

    Clean: Remove .archive folder from dev submodule

--- .archive/2026-01-26-goblin-nuclear-clean/goblin/src/lib/typo/prettier.config.js
+++
@@ -1,7 +0,0 @@
-import config from "@robino/prettier";
-
-config.plugins.push("prettier-plugin-svelte", "prettier-plugin-tailwindcss");
-
-config.overrides = [{ files: "*.svelte", options: { parser: "svelte" } }];
-
-export default config;
